8cbe8860411ce376de599a961130f8a8538866ad
ken restivo <ken@restivo.org>
2015-11-04 14:41:15 -0800

    Cleaner

--- converse.php
+++ converse.php
@@ -110,14 +110,13 @@ function converse_settings_post($a,&$post) {
         if(! local_channel())
 		return;
 	
-	if(is_site_admin()){
-		if($_POST['converse-submit']) {
-			set_pconfig(local_channel(),'converse','enable',intval($_POST['converse']));
-			info( t('Converse Settings updated.') . EOL);
-			set_config('converse','bosh_path',trim($_POST['bosh_path']));
-			set_config('converse','websockets_path',trim($_POST['websockets_path']));
-		}
+	if(is_site_admin() && $_POST['converse-submit']) {
+		set_pconfig(local_channel(),'converse','enable',intval($_POST['converse']));
+		info( t('Converse Settings updated.') . EOL);
+		set_config('converse','bosh_path',trim($_POST['bosh_path']));
+		set_config('converse','websockets_path',trim($_POST['websockets_path']));
 	}
+
 
 
 }
