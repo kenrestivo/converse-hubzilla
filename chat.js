980867e20fddbd291a2e77ea1e6d73e3a6d08039
ken restivo <ken@restivo.org>
2015-11-04 02:06:46 -0800

    Working proof of concept.

--- chat.js
+++ chat.js
@@ -1,14 +1,15 @@
-converse.initialize({
-    bosh_service_url: 'https://hub.spaz.org:5281/http-bind/',
-    websocket_url: 'wss://hub.spaz.org:5281/websocket/',
-    keepalive: true,
-    message_carbons: true,
-    debug: true, /// not for production
-    play_sounds: true,
-    roster_groups: true,
-    show_controlbox_by_default: true,
-    xhr_user_search: false
-});
+$(document).ready(function() {
+    converse.initialize({
+	bosh_service_url: 'https://hub.spaz.org:5281/http-bind/',
+	websocket_url: 'wss://hub.spaz.org:5281/websocket/',
+	keepalive: true,
+	message_carbons: true,
+	debug: true, /// not for production
+	play_sounds: true,
+	roster_groups: true,
+	show_controlbox_by_default: true,
+	xhr_user_search: false
+    })});
 
 
 
