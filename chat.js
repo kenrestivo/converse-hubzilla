d0d5e255a736a32738f1b53a9db3da1387c9dfdd
ken restivo <ken@restivo.org>
2015-11-04 01:54:39 -0800

    Add libraries

---
+++ chat.js
@@ -0,0 +1,12 @@
+require(['converse'], function (converse) {
+    converse.initialize({
+        bosh_service_url: 'https://hub.spaz.org:5281/http-bind/',
+        websocket_url: 'wss://hub.spaz.org:5281/websocket/',
+        keepalive: true,
+        message_carbons: true,
+        play_sounds: true,
+        roster_groups: true,
+        show_controlbox_by_default: true,
+        xhr_user_search: false
+    });
+});
