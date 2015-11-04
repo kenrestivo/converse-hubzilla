35f06ee82220ddbfdbb28b9c9c1c3df7e4fe5488
ken restivo <ken@restivo.org>
2015-11-03 22:13:14 -0800

    Initial

---
+++ converse.php
@@ -0,0 +1,22 @@
+<?php
+
+
+function converse_load(){
+
+}
+
+
+function converse_unload(){
+
+}
+
+
+function converse_content(&$a){
+    $a->page['htmlhead'] .= 
+  $a->get_baseurl()
+}
+
+
+function convese_module() {
+	return;
+}
