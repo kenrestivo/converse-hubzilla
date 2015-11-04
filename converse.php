211c3beb380917d4f336c4e680b452f9342bf2e9
ken restivo <ken@restivo.org>
2015-11-04 01:05:48 -0800

    Some thrashing around js/jquery conflicts

--- converse.php
+++ converse.php
@@ -21,10 +21,16 @@ function converse_unload(){
 
 
 function converse_content(&$a, &$b){
+
+
+	// head_add_js and head_add_css would be so much cleaner, but don't work here for some reason.
 	$a->page['htmlhead'] .=  '<link rel="stylesheet" href="' .   
 		$a->get_baseurl() . 
-		"/addon/converse/converse.min.js" .'" media="all" />';
-    
+		"/addon/converse/converse.min.css" .'" media="all" />';
+
+	return;
+	
+	/// XXX this breaks redbasic.js for some reason
 	$a->page['htmlhead'] .=   '<script src="' . 
 		$a->get_baseurl() . '/addon/converse/converse.min.js' .
 		'"></script>';
