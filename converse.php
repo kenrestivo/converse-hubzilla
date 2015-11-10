62fcb919182aa1da0de44cd032ac683cce6604b3
ken restivo <ken@restivo.org>
2015-11-10 02:26:15 -0800

    Prebind for persistence through page reloads and lighter-weight sign-on. Add username/password (plaintext for now) in database. Remove websockets; it won't work through page reloads.

--- converse.php
+++ converse.php
@@ -3,15 +3,15 @@
 /**
  * Name: Converse XMPP Chat plugin
  * Description: Enables XMPP chat with Converse.js
- * Version: 0.1
+ * Version: 0.2
  * Author: ken restivo <ken@restivo.org>
  */
 
-
+require_once('XMPP-BOSH-toolkit/lib/XmppBosh.php');
 
 
 function converse_load(){
-	register_hook('construct_page', 'addon/converse/converse.php', 'converse_content');
+	register_hook('construct_page', 'addon/converse/converse.php', 'converse_all_pages');
 
 	register_hook('feature_settings', 'addon/converse/converse.php', 'converse_settings');
 	register_hook('feature_settings_post', 'addon/converse/converse.php', 'converse_settings_post');
@@ -24,15 +24,13 @@ function converse_unload(){
 }
 
 
-function converse_content(&$a, &$b){
+function converse_all_pages(&$a, &$b){
 	// chat's really only for us
 	if(! local_channel())
 		return;
 
 	$active = get_pconfig(local_channel(), 'converse', 'enable');
 	$bosh_url = get_config('converse','bosh_url');
-	$websockets_url = get_config('converse','websockets_url');
-	// TODO: add domain placeholder
 	
         if(! $active){
 		return;
@@ -53,34 +51,33 @@ function converse_content(&$a, &$b){
 	// vars documented here https://conversejs.org/docs/html/configuration.html
 	$a->page['content'] .= '<script language="javascript" type="text/javascript">' .
 		"require(['converse'], function (converse) {
-    converse.initialize({
-	bosh_service_url: '$bosh_url/'," .
-		(($websockets_url !== '') ? "websocket_url: '$websockets_url/' , " : "") .
-		"//domain_placeholder: '', /// TODO add to settings
-	keepalive: true,
-	animate: false,
-	autologin: false, // will be true once jid is populated, WHEN it is populated
-	// TODO: provide jid, password, and auto-log them in (pconfig, auto-populate from db)
-	message_carbons: true,
-	debug: false,  // TODO add to settings (pconfig? config?)
-	play_sounds: true, // TODO: let the user decide (pconfig)
-	roster_groups: true,
-	show_controlbox_by_default: false, //TODO: add to pconfig
-	xhr_user_search: false
-    })});" .
-		';</script>';
+	$.ajax({
+		type:'GET',
+		url: '" . $a->get_baseurl() . "/converse/config'," .
+		"dataType: 'json',
+		success: function(data){ 
+                  converse.initialize(data);
+                 }})});" .
+		'</script>';
 	// NOTE: there's no additional content necessary, the JS above loads everything needed.
+	
+	//$a->page['content'] .=  "foooo";
 }
 
 
 
+
 function converse_settings(&$a,&$s) {
+
+	// Shouldn't these only show up if the plugin is enabled?
 
 	if(! local_channel())
 		return;
 
 
 
+	$username = get_pconfig(local_channel(),'converse','username');
+	$password = get_pconfig(local_channel(),'converse','password');
 
 	$enabled = get_pconfig(local_channel(),'converse','enable');
    
@@ -88,7 +85,7 @@ function converse_settings(&$a,&$s) {
 
 
 	$sc .= replace_macros(get_markup_template('field_checkbox.tpl'), array(
-				      '$field'	=> array('converse', 
+				      '$field'	=> array('enable', 
 							 t('Enable Converse.js XMPP Chat Plugin' .
 							   is_site_admin() ? "Enable/disable for the ADMIN user (does not affect other users)" : ""), 
 							 $checked, 
@@ -96,20 +93,35 @@ function converse_settings(&$a,&$s) {
 							 array(t('No'),
 							       t('Yes')))));
 
+	$sc .= replace_macros(get_markup_template('field_input.tpl'), 
+			      array('$field' => array('username', 
+						      t('Username (with no @)'), 
+						      $username, 
+						      t('Jabber username'))));
+
+	$sc .= replace_macros(get_markup_template('field_input.tpl'), 
+			      array('$field' => array('password', 
+						      t('Password (with no @)'), 
+						      $password, 
+						      t('Jabber password'))));
+
+
 
 	if( is_site_admin() ){
+		$domain = get_config('converse','domain');
 		$bosh_url = get_config('converse','bosh_url');
 		$sc .= replace_macros(get_markup_template('field_input.tpl'), 
 				      array('$field' => array('bosh_url', 
 							      t('URL to BOSH host.'), 
 							      $bosh_url, 
 							      t('Full URL, with http:// or https://, and /http-bind at the end'))));
-		$websockets_url = get_config('converse','websockets_url');
+
 		$sc .= replace_macros(get_markup_template('field_input.tpl'), 
-				      array('$field' => array('websockets_url', 
-							      t('URL to websockets host.  Leave blank to not use Websockets.'), 
-							      $websockets_url, 
-							      t('Full URL, with ws:// or wsss://, and websocket or whatever at the end'))));
+				      array('$field' => array('domain', 
+							      t('XMPP Domain.'), 
+							      $domain, 
+							      t('The domain of your XMPP server.'))));
+
 	}
 				      
 
@@ -127,14 +139,78 @@ function converse_settings_post($a,&$post) {
         if(! local_channel())
 		return;
 
-	set_pconfig(local_channel(),'converse','enable',intval($_POST['converse']));
+	set_pconfig(local_channel(),'converse','enable',intval($_POST['enable']));
+	set_pconfig(local_channel(),'converse','username',$_POST['username']);
+	set_pconfig(local_channel(),'converse','password',$_POST['password']);
 	
 	if(is_site_admin() && $_POST['converse-submit']) {
+		set_config('converse','domain',trim($_POST['domain']));
 		set_config('converse','bosh_url',trim($_POST['bosh_url']));
-		set_config('converse','websockets_url',trim($_POST['websockets_url']));
 		info( t('Converse Settings updated.') . EOL);
 	}
 
 }
 
+
+function converse_content(&$a){
+	// TODO: shadow the admin settings here
+	return "Converse XMPP chat module";
+}
+
+
+function prebind(){
+	/// XXX holder
+
+	$username = get_pconfig(local_channel(),'converse','username');
+	$password = get_pconfig(local_channel(),'converse','password');
+
+
+	$domain = get_config('converse','domain');
+	$bosh_url = get_config('converse','bosh_url');
+
+	$xmppBosh = new XmppBosh($domain, $bosh_url, 
+				 'converse-hubzilla', // TODO: must have unique per session?
+				 ((strpos($bosh_url, 'https://') > 0) ? true : false));
+	$xmppBosh->connect($username, $password);
+	return $xmppBosh->getSessionInfo();
+
+}
+
+
+
+function converse_init(&$a) {
+        if(! local_channel())
+		return;
+	
+	$x = argc(); 
+	if($x > 1){
+		switch(argv(1)){
+		case "prebind":
+			json_return_and_die(prebind());
+			break;
+		case "config":
+			$bosh_url = get_config('converse','bosh_url');
+			$domain = get_config('converse','domain');
+			$username = get_pconfig(local_channel(), 'converse', 'username');
+			json_return_and_die(
+				array("bosh_service_url" => $bosh_url,
+				      // "domain_placeholder" => '' /// TODO add to settings
+				      "keepalive" => true,
+				      "prebind_url" =>  $a->get_baseurl() . '/converse/prebind',
+				      "animate" => false,
+				      'jid' => $username . '@' . $domain, /// XXX resource needed TODO
+				      "authentication" => 'prebind',
+				      "autologin" => true,
+				      "message_carbons" => true,
+				      "debug" => false,  // TODO add to settings (pconfig? config?)
+				      "play_sounds" => true, // TODO: let the user decide (pconfig)
+				      "roster_groups" => true,
+				      "show_controlbox_by_default" => false, //TODO: add to pconfig
+				      "xhr_user_search" => false));
+			break;
+		}
+	}
+}
+
+function converse_module() { return; }
 
